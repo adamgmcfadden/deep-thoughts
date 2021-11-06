cb5634b5f0e95fdf6dda64862b3dccbd3e2e7ead
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-11-05 20:59:18 -0400

    changed a few things - deployment is proving difficult

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
+// import dependencies -- express, apollo server
 const express = require("express");
-// import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
+// import db config
 const db = require("./config/connection");
-const { authMiddleware } = require("./utils/auth");
 
+const { authMiddleware } = require("./utils/auth");
+const path = require("path");
+
+// port - process.ENV or 3001
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -28,7 +32,7 @@ const startServer = async () => {
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
-// serve up static assets
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
